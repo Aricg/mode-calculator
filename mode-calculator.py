1eaae4583ce2cad547f7e2d6cbb37d2e120f5957
Aric Gardner <actual@aricgardner.com>
2023-07-22 11:32:54 -0400

    feature: pick current leader

--- mode-calculator.py
+++ mode-calculator.py
@@ -11,13 +11,19 @@ def mode():
 class ModeCounter:
   def __init__(self):
       self.frequency_dictonary = {}
+      self.winning_frequency = 0
 
   def add_number(self,number):
     if number not in self.frequency_dictonary:
         self.frequency_dictonary[number] = 1
     else:
-        self.frequency_dictonary[number] +=1
+        self.frequency_dictonary[number] += 1
+    self.update(number)
 
- # def update(self, number):
+  def update(self, number):
+      current_freqency = self.frequency_dictonary[number]
+      if current_freqency > self.winning_frequency:
+          self.winning_frequency = current_freqency
+
  #def get(self):
 
